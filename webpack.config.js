fa4b69aa71c8610a677ab46360f79e21622d2694
Arkadiy Kotlyarov <test@mail.com>
2021-02-03 09:44:38 +0400

    add configuration

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const UglifyjsWebpackPlugin = require("uglifyjs-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "src/app/index.tsx"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[hash].bundle.js",
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.resolve(__dirname, "./dist"),
+    open: true,
+    compress: true,
+    hot: true,
+    port: 8080,
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-typescript"],
+              plugins: ["@babel/plugin-proposal-class-properties"],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.tsx$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                "@babel/preset-env",
+                "@babel/preset-react",
+                "@babel/preset-typescript",
+              ],
+              plugins: ["@babel/plugin-proposal-class-properties"],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, "src/app/index.html"),
+      favicon: path.resolve(__dirname, "src/app/favicon.ico"),
+    }),
+    new CleanWebpackPlugin(),
+    new UglifyjsWebpackPlugin(),
+  ],
+};
